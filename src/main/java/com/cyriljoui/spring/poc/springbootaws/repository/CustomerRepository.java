86181c225a91bdb0f8dfc2711c96a86c6b6e7376
Cyril Joui <cyril.joui@gmail.com>
2015-11-04 14:34:29 +0100

    Rest Data Repository - search requests added

--- src/main/java/com/cyriljoui/spring/poc/springbootaws/repository/CustomerRepository.java
+++ src/main/java/com/cyriljoui/spring/poc/springbootaws/repository/CustomerRepository.java
@@ -2,8 +2,16 @@ package com.cyriljoui.spring.poc.springbootaws.repository;
 
 import com.cyriljoui.spring.poc.springbootaws.model.Customer;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+
+    List<Customer> findByLastname(@Param("lastname") String lastname);
+
+    List<Customer> findByFirstname(@Param("firstname") String firstname);
+
 }
